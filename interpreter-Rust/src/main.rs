bcfcbd783e66de87bce96db41a0dfba8a325eec5
Felipe Henrique Camargos Ferreira <felipeferreira.sp.dev@gmail.com>
2023-10-14 09:40:19 -0300

    feat: adiciona variaveis

--- interpreter-Rust/src/main.rs
+++ interpreter-Rust/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(unused)]
-use std::{fs, io::{stdin, Read}};
+use std::{fs, io::{stdin, Read}, collections::HashMap};
 
 use serde::Deserialize;
 
@@ -69,9 +69,11 @@ pub enum Term {
     Print(Print),
     Binary(Binary),
     If(If),
+    Let(Let),
+    Var(Var),
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Val {
     Void,
     Int(i32),
@@ -79,13 +81,32 @@ pub enum Val {
     Str(String),
 }
 
-fn eval(term: Term) -> Val {
+#[derive(Debug, Deserialize)]
+pub struct Parameter {
+    text: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct  Let {
+    name: Parameter,
+    value: Box<Term>,
+    next: Box<Term>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct  Var {
+    text: String,
+}
+
+pub type Scope = HashMap<String, Val>;
+
+fn eval(term: Term, scope: &mut Scope) -> Val {
     match term {
         Term::Bool(bool) => Val::Bool(bool.value),
         Term::Int(number) => Val::Int(number.value),
         Term::Str(str) => Val::Str(str.value),
         Term::Print(print) => {
-            let val = eval(*print.value);
+            let val = eval(*print.value, scope);
             match val {
                 Val::Int(n) => println!("{}", n),
                 Val::Bool(b) => println!("{}", b),
@@ -97,8 +118,8 @@ fn eval(term: Term) -> Val {
         Term::Binary(bin) => {
             match bin.op {
                 BinaryOp::Add => {
-                    let lhs = eval(*bin.lhs);
-                    let rhs = eval(*bin.rhs);
+                    let lhs = eval(*bin.lhs, scope);
+                    let rhs = eval(*bin.rhs, scope);
                     match (lhs, rhs) {
                         (Val::Int(a), Val::Int(b)) => Val::Int(a + b),
                         (Val::Int(a), Val::Str(s)) => Val::Str(format!("{a}{s}")),
@@ -107,32 +128,32 @@ fn eval(term: Term) -> Val {
                         _ => panic!("Invalid value"),
                     }},
                 BinaryOp::Sub => {
-                    let lhs = eval(*bin.lhs);
-                    let rhs = eval(*bin.rhs);
+                    let lhs = eval(*bin.lhs, scope);
+                    let rhs = eval(*bin.rhs, scope);
                     match (lhs, rhs) {
                         (Val::Int(a), Val::Int(b)) => Val::Int(a - b),
                         _ => panic!("Invalid value"),
                     }
                 }
                 BinaryOp::Mul => {
-                    let lhs = eval(*bin.lhs);
-                    let rhs = eval(*bin.rhs);
+                    let lhs = eval(*bin.lhs, scope);
+                    let rhs = eval(*bin.rhs, scope);
                     match (lhs, rhs) {
                         (Val::Int(a), Val::Int(b)) => Val::Int(a * b),
                         _ => panic!("Invalid value"),
                     }
                 }
                 BinaryOp::Div => {
-                    let lhs = eval(*bin.lhs);
-                    let rhs = eval(*bin.rhs);
+                    let lhs = eval(*bin.lhs, scope);
+                    let rhs = eval(*bin.rhs, scope);
                     match (lhs, rhs) {
                         (Val::Int(a), Val::Int(b)) => Val::Int(a / b),
                         _ => panic!("Invalid value"),
                     }
                 }
                 BinaryOp::Rem => {
-                    let lhs = eval(*bin.lhs);
-                    let rhs = eval(*bin.rhs);
+                    let lhs = eval(*bin.lhs, scope);
+                    let rhs = eval(*bin.rhs, scope);
                     match (lhs, rhs) {
                         (Val::Int(a), Val::Int(b)) => Val::Int(a % b),
                         _ => panic!("Invalid value"),
@@ -141,10 +162,24 @@ fn eval(term: Term) -> Val {
             }
         },
         Term::If(i) => {
-            match eval(*i.condition) {
-                Val::Bool(true) => eval(*i.then),
-                Val::Bool(false) => eval(*i.otherwise),
+            match eval(*i.condition, scope) {
+                Val::Bool(true) => eval(*i.then, scope),
+                Val::Bool(false) => eval(*i.otherwise, scope),
                 _ => panic!("Invalid value"),
+            }
+        }
+        Term::Let(l) => {
+            let name = l.name.text;
+            let value = eval(*l.value, scope);
+
+            scope.insert(name, value);
+
+            eval(*l.next, scope)
+        }
+        Term::Var(v) => {
+            match scope.get(&v.text) {
+                Some(val) => val.clone(),
+                None => panic!("Invalid value"),
             }
         }
     }
@@ -157,5 +192,6 @@ fn main() {
     let program = serde_json::from_str::<File>(&program).unwrap();
     
     let term = program.expression;
-    eval(term);
+    let mut scope = HashMap::new();
+    eval(term, &mut scope);
 }
